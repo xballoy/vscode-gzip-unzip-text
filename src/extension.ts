a7aa9ad8c8950d48cb29bd7b2913d07ed2f3b8de
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2024-05-29 16:54:43 -0400

    style: run biomejs on project

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import { commands } from 'vscode';
-import { gzipSelectedText, unzipSelectedText } from './core';
 import type { ExtensionContext } from 'vscode';
+import { gzipSelectedText, unzipSelectedText } from './core';
 
 export const activate = (context: ExtensionContext) => {
   context.subscriptions.push(commands.registerCommand('gzip-unzip-text.gzip', gzipSelectedText));
