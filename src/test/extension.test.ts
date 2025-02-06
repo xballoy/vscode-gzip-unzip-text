f2c6f0b8143413839699752a6275aa66814c7cd2
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2025-02-06 17:17:50 -0500

    chore: fix linting

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,14 +1,14 @@
 import { strictEqual } from 'node:assert';
+import fc from 'fast-check';
 import { gzipText } from '../core/gzipText';
 import { unzipText } from '../core/unzipText';
-import fc from 'fast-check';
 
 suite('Gzip / Unzip text Test Suite', () => {
   test('should be reversible', () => {
     fc.assert(
       fc.property(fc.string(), (text) => {
         strictEqual(unzipText(gzipText(text)), text);
-      })
-    )
+      }),
+    );
   });
 });
