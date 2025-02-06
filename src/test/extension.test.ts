516c6757fda80706cc6709f29c05da9b5a9a5314
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2025-02-06 16:59:45 -0500

    test: use fast-check to improve tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,11 +1,14 @@
 import { strictEqual } from 'node:assert';
 import { gzipText } from '../core/gzipText';
 import { unzipText } from '../core/unzipText';
+import fc from 'fast-check';
 
 suite('Gzip / Unzip text Test Suite', () => {
   test('should be reversible', () => {
-    const input = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 👍';
-
-    strictEqual(unzipText(gzipText(input)), input);
+    fc.assert(
+      fc.property(fc.string(), (text) => {
+        strictEqual(unzipText(gzipText(text)), text);
+      })
+    )
   });
 });
