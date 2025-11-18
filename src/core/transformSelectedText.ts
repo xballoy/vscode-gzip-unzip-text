4801879c6bfac37f160de5eba24057f556af364f
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2025-11-18 10:32:28 -0500

    refactor: use await instead of chaining then

--- src/core/transformSelectedText.ts
+++ src/core/transformSelectedText.ts
@@ -1,18 +1,18 @@
 import { getSelectedText, insertTextInNewDocument, showErrorMessage, showInformationMessage } from '../vscode-utils';
 
 export type TextTransformFn = (value: string) => string;
-export const transformSelectedText = (fn: TextTransformFn, operation: string) => {
+export const transformSelectedText = async (fn: TextTransformFn, operation: string) => {
   const selectedText = getSelectedText();
   if (!selectedText) {
-    showInformationMessage(`Select a text to ${operation}`);
+    await showInformationMessage(`Select a text to ${operation}`);
     return;
   }
 
   try {
     const result = fn(selectedText);
-    insertTextInNewDocument(result);
+    await insertTextInNewDocument(result);
   } catch (e) {
     console.error(e);
-    showErrorMessage(`Unable to ${operation} selected text`);
+    await showErrorMessage(`Unable to ${operation} selected text`);
   }
 };
