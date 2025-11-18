6925e2ef2485732c36c73a7b45aca0bafeffd322
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2025-11-18 10:09:47 -0500

    chore: remove unused dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,8 @@ import { commands } from 'vscode';
 import { gzipSelectedText, unzipSelectedText } from './core';
 
 export const activate = (context: ExtensionContext) => {
-  context.subscriptions.push(commands.registerCommand('gzip-unzip-text.gzip', gzipSelectedText));
-  context.subscriptions.push(commands.registerCommand('gzip-unzip-text.unzip', unzipSelectedText));
+  context.subscriptions.push(
+    commands.registerCommand('gzip-unzip-text.gzip', gzipSelectedText),
+    commands.registerCommand('gzip-unzip-text.unzip', unzipSelectedText),
+  );
 };
